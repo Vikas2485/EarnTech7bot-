6dbb002271cc4c48e84d0ff28f14b033a4b7bd65
Vikas2485 <absurdtech.media@gmail.com>
2025-06-22 01:48:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
-TOKEN = "YOUR_BOT_TOKEN"
-ADMIN_ID = "@Vikas7685"
-CHANNEL_ID = "@EarningCash7"
-INSTAGRAM_LINK = "https://www.instagram.com/earningcash7"
+# config.py
+
+TOKEN = "8194794683:AAGoC_Uc01TtC6-tX-uVYEBxJYDCX6wpegQ"
+ADMIN_ID = 5296574124
+CHANNEL_ID = "@earningcash7"
+INSTAGRAM_LINK = "https://instagram.com/earningcash7"
